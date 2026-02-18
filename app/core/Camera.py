ee1589e1015c9d106aeb3fe1f7ac1f95af0dd0de
StarAres1 <titanolimpa5@gmail.com>
2026-02-18 14:18:41 +0300

    Добавлена возможность записи видео в файл avi

--- app/core/Camera.py
+++ app/core/Camera.py
@@ -1,10 +1,17 @@
 import cv2
+from datetime import datetime
 
 class Camera:
     def __init__(self, index, name):
         self.cap = None
         self.index = index
         self.name = name
+        self.flag_record = False
+        self.fourcc = None
+        self.width = None
+        self.height = None
+        self.output = None
+        self.fps = None
 
         self.connect()
 
@@ -14,18 +21,28 @@ class Camera:
 
 
     def disconnect(self):
+        if self.flag_record:
+            self.stop_record()
+
         self.cap.release()
 
 
     def capture_video(self):
-        if not self.cap.isOpened:
-            return
+
+        if not self.get_property():
+            return False
+
+        # Следующая строка временно!!!! До момента создания GUI
+        self.start_record()
         try:
             while True:
                 ret, frame = self.cap.read()
 
                 if not ret:
                     continue
+
+                if self.flag_record:
+                    self.write_video(frame)
 
                 cv2.imshow(self.name, frame)
 
@@ -36,4 +53,44 @@ class Camera:
         finally:
             self.disconnect()
 
-        
+    def start_record(self):
+        self.flag_record = True
+
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID') # для формата avi
+        filename = f'video_output/{self.name}_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.avi'
+        self.output = cv2.VideoWriter(
+            filename.replace(" ", "_"),
+            self.fourcc,
+            self.fps,
+            (self.width, self.height))
+
+    def stop_record(self):
+        self.flag_record = False
+        self.fourcc = None
+
+        self.output.release()
+        self.output = None
+
+    def write_video(self, frame):
+        if self.output == None:
+            return
+
+        self.output.write(frame)
+
+    def get_property(self):
+        if not self.cap.isOpened():
+            return False
+
+        ret, frame = self.cap.read()
+
+        if not ret:
+            return False
+
+        self.height, self.width, channel = frame.shape
+        print("channel: ", channel)
+
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+        print("fps: ", self.fps)
+
+        print("Свойства получены")
+        return True
--- main.py
+++ main.py
@@ -11,8 +11,16 @@ if __name__ == "__main__":
     print(connected_camera)
 
     camera1 = cameraManager.create_camera(connected_camera[number]["index"], connected_camera[number]["name"])
-    camera2 = cameraManager.create_camera(connected_camera[1]["index"], connected_camera[1]["name"])
+    camera1.capture_video()
 
+
+
+# Для многопоточной обработки
+""" 
+
+    camera1 = cameraManager.create_camera(connected_camera[number]["index"], connected_camera[number]["name"])
+    camera2 = cameraManager.create_camera(connected_camera[1]["index"], connected_camera[1]["name"])
+    
     t1 = threading.Thread(target=camera1.capture_video)
     t2 = threading.Thread(target=camera2.capture_video)
 
@@ -21,3 +29,4 @@ if __name__ == "__main__":
 
     t1.join()
     t2.join()
+"""
